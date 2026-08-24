7e59073b921f87f8374b5197245e1c5d0c5706c0
Congee <guswogus81@gmail.com>
2026-08-24 13:48:23 +0900

    fix(frontend): jest가 e2e 디렉토리를 테스트로 잡지 않도록 제외
    저장소 경로에 포함된 "[02]"가 testPathIgnorePatterns 정규식에서
    문자클래스로 해석돼 <rootDir> 절대경로 패턴이 매치되지 않는 문제도
    같이 발견해 부분 문자열 패턴으로 바꿨다.

--- apps/frontend/jest.config.js
+++ apps/frontend/jest.config.js
@@ -5,6 +5,9 @@ const createJestConfig = nextJest({ dir: "./" });
 /** @type {import('jest').Config} */
 const customJestConfig = {
   testEnvironment: "jest-environment-jsdom",
+  // <rootDir>로 절대경로를 넣으면 이 저장소 경로에 포함된 "[02]"가 정규식 문자클래스로
+  // 해석돼 매치가 깨진다(testPathIgnorePatterns는 정규식). 부분 문자열로만 지정한다.
+  testPathIgnorePatterns: ["/node_modules/", "/e2e/"],
 };
 
 module.exports = createJestConfig(customJestConfig);
